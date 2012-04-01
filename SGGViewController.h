3cdff6b2e43786e52e7f2885201ed831b066f089
Chris Hansen <hansec@uw.edu>
2012-03-31 19:09:56 -0700

    - Setup first stage of iPhone 2D game tutorial

---
+++ SGGViewController.h
@@ -0,0 +1,13 @@
+//
+//  SGGViewController.h
+//  SimpleGLKitGame
+//
+//  Created by Chris Hansen on 3/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface SGGViewController : GLKViewController
+
+@end
