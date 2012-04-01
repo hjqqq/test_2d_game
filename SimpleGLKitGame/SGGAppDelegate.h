7dc575e780c03228a34e38c48eb873536759b569
Chris Hansen <hansec@uw.edu>
2012-03-31 18:48:31 -0700

    Initial Commit

---
+++ SimpleGLKitGame/SGGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SGGAppDelegate.h
+//  SimpleGLKitGame
+//
+//  Created by Chris Hansen on 3/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleGLKitGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleGLKitGame
+//
+//  Created by Chris Hansen on 3/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGGAppDelegate class]));
+    }
+}
